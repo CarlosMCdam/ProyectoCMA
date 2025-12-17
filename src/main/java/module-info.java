a829965ed4509cc2fa775df5a0f498533a660150
CarlosMCdam <carlos.martinez79@educa.madrid.org>
2025-12-17 19:49:10 +0100

    Creación del proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module cma.proyectocma {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens cma.proyectocma to javafx.fxml;
+    exports cma.proyectocma;
+}
